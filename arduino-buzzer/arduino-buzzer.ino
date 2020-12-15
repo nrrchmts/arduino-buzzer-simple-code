a8d379fb192fce76324a2b3570bcca419872dbd6
Nurrochmat Saptoaji <nurrochmat.sapto@gmail.com>
2020-12-16 06:09:20 +0700

    updat3 code
    buzzer only and buzzer with pcb circuit its different haha

--- arduino-buzzer/arduino-buzzer.ino
+++ arduino-buzzer/arduino-buzzer.ino
@@ -6,8 +6,13 @@ Serial.begin(9600);          //baudrate arduino atau Serial komunikasi
 }
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(buzzer,HIGH); //buzzer bunyi
-delay(1000);   //dengan waktu 1000 ms
-digitalWrite(buzzer,LOW); //buzzer mati
-delay(1000);   //dengan waktu 1000 ms
+  digitalWrite(buzzer,HIGH); //buzzer bunyi
+  delay(1000);   //dengan waktu 1000 ms
+  digitalWrite(buzzer,LOW); //buzzer mati
+  delay(1000);   //dengan waktu 1000 ms
+  
+  // with tone or use low level trigger
+  //8, 500, 1000, 2000, 3000
+  tone(buzzer, 8); // tone() is the main function to use with a buzzer, it takes 2 or 3 parameteres (buzzer pin, sound frequency, duration)
+  delay(1000);
 }
